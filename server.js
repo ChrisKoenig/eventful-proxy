c338720df64aa227a2bde1c73de481bb44daeb9a
unknown <tacowan@MININT-TL4N53C.northamerica.corp.microsoft.com>
2014-01-07 13:09:09 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+//http://localhost:1235/eventful/rest/events/search?location=dallas&sort_order=popularity
+var express = require('express');
+var azure = require('azure');
+var request = require('request')
+var accountName = 'your storage account';
+var accountKey = 'your secret key';
+
+var app = express();
+
+//Example request: http://localhost:1235/api?location=dallas&sort_order=popularity
+// change "yourkeygoeshere" to your own Eventful.com api key
+
+var apiUrl = "http://api.eventful.com/"
+app.use('/eventful', function (req, res) {
+    url = apiUrl + req.url + "&app_key=youreventfulappkey";
+    req.pipe(request(url)).pipe(res);
+});
+
+app.use('/cache', function (req, res) {
+    var blobname = req.query.blobname;
+    url = apiUrl + req.url + "&app_key=youreventfulappkey";
+    res.send("success");
+    request.get(url, function (err, response, body) {
+        if (err) {
+            console.log('Unable to connect.' + err);
+        } else if (response.statusCode !== 200) {
+            console.log('response=' + response.statusCode);
+        } else {
+            save(body, blobname);
+        }
+    });
+    
+});
+
+
+function save(body, blobname) {
+    var containerName = 'austinevents';
+    var blobService = azure.createBlobService(accountName, accountKey);
+    console.log(body)
+    blobService.createBlockBlobFromText(containerName, blobname, body, { contentEncoding: 'UTF-8', contentType: 'application/json' }, function (error) {
+        if (!error) {
+            // File has been uploaded
+        }
+    });
+}
+
+
+app.listen(process.env.PORT || 1235);
